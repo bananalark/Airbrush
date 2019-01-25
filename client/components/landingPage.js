fc39fc6766c9e0f933152c14205a1bceb3bf2b19
Laura <laura.waters22@gmail.com>
2019-01-25 15:12:17 -0500

    initial commit

---
+++ client/components/landingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Typeography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+
+const LandingPage = () => {
+  return (
+    <Grid>
+      <header>HI</header>
+    </Grid>
+  )
+}
+
+export default LandingPage
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,13 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, CameraComponent} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  CameraComponent,
+  LandingPage
+} from './components'
 import {me} from './store'
 
 /**
@@ -19,6 +25,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route exact path="/" component={LandingPage} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/camera" component={CameraComponent} />
