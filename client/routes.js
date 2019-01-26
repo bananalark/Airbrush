350e6023e55d9114093d515a7d69c8db6b8d92c1
Amber Rodriguez <amber.rodriguez0711@gmail.com>
2019-01-26 17:02:17 -0500

    sorry! removed voicecomponent from routes as well!

--- client/routes.js
+++ client/routes.js
@@ -2,13 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {
-  Login,
-  Signup,
-  UserHome,
-  CameraComponent,
-  VoiceComponent
-} from './components'
+import {Login, Signup, UserHome, CameraComponent} from './components'
 import {me} from './store'
 
 /**
