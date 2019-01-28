3fb51cde3015be3318028db03b1b44bccb3a8b7c
Wenyi Zheng <hilarywenyi@gmail.com>
2019-01-28 13:16:34 -0500

    fix: added back cameraComponents.js

--- client/components/cameraComponent.js
+++ client/components/cameraComponent.js
@@ -1,32 +1,59 @@
-import React from 'react'
-import ColorPicker from './ColorPicker'
+import React, {Component} from 'react'
+import colorPicker from './colorPicker'
 
-const CameraComponent = () => {
-  return (
-    <div>
-      <div id="info" />
-      <div id="main">
-        <video
-          id="video"
-          plays-inline="true"
-          style={{display: 'none'}}
-          // style={{
-          //   '-moz-transform': scaleX(-1),
-          //   '-o-transform': scaleX(-1),
-          //   '-webkit-transform': scaleX(-1),
-          //   transform: scaleX(-1),
-          //   display: none
-          // }}
-        />
-
-        <div style={{position: 'relative'}}>
-          <canvas
-            id="background"
-            style={{position: 'absolute', left: 50, top: 50, zIndex: 0}}
-          />
-          <canvas
-            id="output"
-            style={{position: 'absolute', left: 50, top: 50, zIndex: 1}}
+class CameraComponent extends Component {
+  constructor() {
+    super()
+    this.state = {eraseModeOn: false, voiceModeOn: false}
+    this.toggleEraseMode = this.toggleEraseMode.bind(this)
+    this.toggleVoiceMode = this.toggleVoiceMode.bind(this)
+  }
+  componentDidMount() {
+    require('./camera')
+  }
+  toggleEraseMode() {
+    if (this.state.eraseModeOn === true) {
+      this.setState({eraseModeOn: false})
+    } else {
+      this.setState({eraseModeOn: true})
+    }
+  }
+  toggleVoiceMode() {
+    if (this.state.voiceModeOn === true) {
+      this.setState({voiceModeOn: false})
+    } else {
+      this.setState({voiceModeOn: true})
+    }
+  }
+  render() {
+    let eraserModeOn = this.state.eraseModeOn
+    return (
+      <div>
+        <div id="speech-recognition">
+          <h4>Turn Voice Recognition On/Off.</h4>
+          <p>
+            When voice recognition is ON, say "START" to start painting and
+            "STOP" to... well, stop!
+          </p>
+          {this.state.voiceModeOn === true ? (
+            <button onClick={this.toggleVoiceMode}>Voice Currently ON</button>
+          ) : (
+            <button onClick={this.toggleVoiceMode}>Voice Currently OFF</button>
+          )}
+        </div>
+        <div id="info" />
+        <div id="main">
+          <video
+            id="video"
+            plays-inline="true"
+            style={{display: 'none'}}
+            // style={{
+            //   '-moz-transform': scaleX(-1),
+            //   '-o-transform': scaleX(-1),
+            //   '-webkit-transform': scaleX(-1),
+            //   transform: scaleX(-1),
+            //   display: none
+            // }}
           />
           <div id="buttons">
             <h5>Erase Tools!</h5>
@@ -46,20 +73,20 @@ const CameraComponent = () => {
           <div style={{position: 'relative'}}>
             <canvas
               id="background"
-              style={{position: 'absolute', left: 0, top: 0, zIndex: 0}}
+              style={{position: 'absolute', left: 50, top: 50, zIndex: 0}}
             />
             <canvas
               id="output"
-              style={{position: 'absolute', left: 0, top: 0, zIndex: 1}}
+              style={{position: 'absolute', left: 50, top: 50, zIndex: 1}}
             />
           </div>
         </div>
-        <ColorPicker />
+        <colorPicker />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default CameraComponent
 
-require('./camera')
+//require('./camera')
