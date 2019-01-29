61389551876e049f090dbada2cfa57233aa69c86
Laura <laura.waters22@gmail.com>
2019-01-29 16:04:33 -0500

    fix: draw button fixed

--- client/components/utils/draw.js
+++ client/components/utils/draw.js
@@ -19,7 +19,6 @@ import * as tf from '@tensorflow/tfjs'
 const paper = require('paper')
 import clearCanvas from './clearCanvas'
 let command = require('../voiceUtils')
-let drawMode = document.getElementById('draw-button').value
 
 export function createProject(window, canvas) {
   paper.install(window)
@@ -54,6 +53,7 @@ export function drawLine(oneKeypoint, path) {
 
 //on-off switch with gesture
 export function draw(keypoints, minPartConfidence) {
+  let drawMode = document.getElementById('draw-button').value
   return (
     (keypoints[10].score >= minPartConfidence &&
       Math.abs(keypoints[10].position.y - keypoints[6].position.y) < 50) ||
