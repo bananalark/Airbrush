a0c556d16af3ce1411c4079e6444aa07914737f8
saecula <krgilb@gmail.com>
2019-01-29 17:46:54 -0500

    chore: tighten back end, remove sockets x

--- server/api/users.js
+++ server/api/users.js
@@ -2,17 +2,30 @@ const router = require('express').Router()
 const {User} = require('../db')
 module.exports = router
 
-const isAdmin = user => true
+//for when we implement user and picture routes, respectively
+async function isAdmin(req, res, next) {
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.user.id
+      }
+    })
+    if (user.isAdmin) return next()
+    else res.redirect('/')
+  } catch (err) {
+    next(err)
+  }
+}
 
-// function isAuthenticated(req, res, next) {
-//   if (process.env.NODE_ENV === 'test' || req.user) {
-//     return next()
-//   } else {
-//     res.redirect('/')
-//   }
-// }
+function isAuthenticated(req, res, next) {
+  if (process.env.NODE_ENV === 'test' || req.user) {
+    return next()
+  } else {
+    res.redirect('/')
+  }
+}
 
-router.get('/', async (req, res, next) => {
+router.get('/', isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
