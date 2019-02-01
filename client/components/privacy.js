39d9bd34ff0b4c607a9be17a3826888732e8119d
saecula <krgilb@gmail.com>
2019-02-01 09:03:22 -0500

    fix: change instagram to facebook in privacy policy

--- client/components/privacy.js
+++ client/components/privacy.js
@@ -8,12 +8,12 @@ export default class Privacy extends React.Component {
 
         <p>
           Airbrush is an interactive canvas that allows users to share their
-          creations on Instagram. Airbrush does not collect photo album data
-          from a users' Instagram account. It will only post a new photo to a
-          user's album when the user consents to post an image through the
-          Airbrush UI. it does not disclose or use private personal information.
-          This policy may be revised as Airbrush continues to develop. Effective
-          January 2019{' '}
+          creations on Facebook. Airbrush does not collect photo album data from
+          a users' Facebook account. It will only post a new photo to a user's
+          album when the user consents to post an image through the Airbrush UI.
+          it does not disclose or use private personal information. This policy
+          may be revised as Airbrush continues to develop. Effective January
+          2019{' '}
         </p>
       </div>
     )
