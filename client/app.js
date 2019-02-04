606a6e07ef026e6875c40a6da34e76ad2edb8510
hilarywenyi <hilarywenyi@gmail.com>
2019-02-04 08:45:31 -0500

    feat: landing page scrolling to about page

--- client/app.js
+++ client/app.js
@@ -1,13 +1,138 @@
 import React from 'react'
-
-import {Navbar} from './components'
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scroller
+} from 'react-scroll'
 import Routes from './routes'
-const App = () => {
-  return (
-    <div>
-      <Routes />
-    </div>
-  )
+import LandingPage from './components/landingPage'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Routes />
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+  // constructor(props){
+  //   super(props)
+  //   this.scrollToTop = this.scrollToTop.bind(this);
+  // }
+
+  componentDidMount() {
+    Events.scrollEvent.register('begin', function() {
+      console.log('begin', arguments)
+    })
+
+    Events.scrollEvent.register('end', function() {
+      console.log('end', arguments)
+    })
+  }
+
+  // scrollToTop() {
+  //   scroll.scrollToTop();
+  // }
+
+  // scrollTo() {
+  //   scroller.scrollTo('scroll-to-element', {
+  //     duration: 800,
+  //     delay: 0,
+  //     smooth: 'easeInOutQuart'
+  //   })
+  // }
+
+  scrollToWithContainer() {
+    let goToContainer = new Promise((resolve, reject) => {
+      Events.scrollEvent.register('end', () => {
+        resolve()
+        Events.scrollEvent.remove('end')
+      })
+
+      scroller.scrollTo('scroll-container', {
+        duration: 800,
+        delay: 0,
+        smooth: 'easeInOutQuart'
+      })
+    })
+
+    goToContainer.then(() =>
+      scroller.scrollTo('scroll-container-second-element', {
+        duration: 800,
+        delay: 0,
+        smooth: 'easeInOutQuart',
+        containerId: 'scroll-container'
+      })
+    )
+  }
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin')
+    Events.scrollEvent.remove('end')
+  }
+
+  render() {
+    return (
+      <div>
+        <LandingPage />
+        <Link
+          activeClass="active"
+          to="firstInsideContainer"
+          spy={true}
+          smooth={true}
+          duration={250}
+          containerId="containerElement"
+          style={{display: 'inline-block', margin: '20px'}}
+        >
+          Start Drawing
+        </Link>
+
+        <Link
+          activeClass="active"
+          to="secondInsideContainer"
+          spy={true}
+          smooth={true}
+          duration={250}
+          containerId="containerElement"
+          style={{display: 'inline-block', margin: '20px'}}
+        >
+          Go to second element inside container
+        </Link>
+
+        <Element
+          name="test7"
+          className="element"
+          id="containerElement"
+          style={{
+            position: 'relative',
+            height: '1200px',
+            overflow: 'scroll',
+            marginBottom: '100px'
+          }}
+        >
+          <Element
+            name="firstInsideContainer"
+            style={{
+              marginBottom: '1200px'
+            }}
+          >
+            first element inside container
+          </Element>
+
+          <Element
+            name="secondInsideContainer"
+            style={{
+              marginBottom: '1200px'
+            }}
+          >
+            second element inside container
+          </Element>
+        </Element>
+      </div>
+    )
+  }
 }
 
 export default App
--- client/components/landingPage.js
+++ client/components/landingPage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Parallaxprovider} from 'react-scroll-parallax'
+import style from './style/landingPage.scss'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
@@ -7,18 +9,20 @@ import {Link} from 'react-router-dom'
 const LandingPage = () => {
   return (
     <div>
-      <Grid container direction="column" justify="center" alignItems="center">
-        <Grid item>
-          <Typography variant="h1">ai rbrush</Typography>
+      <Parallaxprovider>
+        <Grid container direction="column" justify="center" alignItems="center">
+          <Grid item>
+            <Typography variant="h1">Airbrush</Typography>
+          </Grid>
+          <Grid item>
+            <Link to="/camera">
+              <Button>
+                <Typography variant="h5">start drawing</Typography>
+              </Button>
+            </Link>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Link to="/camera">
-            <Button>
-              <Typography variant="h5">start drawing</Typography>
-            </Button>
-          </Link>
-        </Grid>
-      </Grid>
+      </Parallaxprovider>
     </div>
   )
 }
