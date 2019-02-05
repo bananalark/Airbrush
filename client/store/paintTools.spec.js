5f82f4c0669549e12b89f231edd21ee414d297ca
saecula <krgilb@gmail.com>
2019-02-05 14:58:38 -0500

    trav fix

--- client/store/paintTools.spec.js
+++ client/store/paintTools.spec.js
@@ -19,7 +19,7 @@ describe('paintTools reducer', () => {
       voiceModeOn: false,
       drawModeOn: false,
       chosenBrush: 'defaultLine',
-      chosenBodyPart: 'rightHand',
+      chosenBodyPart: 'hand',
       size: 'small'
     })
   })
