15c84029a96a9d4f67443a1c1e3a0d8f57e7cefc
Tom Mao <tommao@Toms-MacBook-Air.local>
2019-02-05 14:44:36 -0500

    feat: switch to work on toolbar UI

--- client/index.js
+++ client/index.js
@@ -4,17 +4,8 @@ import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
-//import App from './app'
-import Fun from './fun'
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Fun />
-    </Router>
-  </Provider>,
-  document.getElementById('app')
-)
-
+import App from './app'
+//import Fun from './fun'
 // ReactDOM.render(
 //   <Provider store={store}>
 //     <Router history={history}>
@@ -23,3 +14,12 @@ ReactDOM.render(
 //   </Provider>,
 //   document.getElementById('app')
 // )
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+)
