e1cdb2eb0b9b6a2f5084d7b91634190866b5b01a
Amber Rodriguez <amber.rodriguez0711@gmail.com>
2019-02-09 18:56:28 -0500

    Added alternatives for speech recognition and general housekeeping

--- client/components/toolbar.js
+++ client/components/toolbar.js
@@ -1,32 +1,6 @@
 import React, {Component} from 'react'
-import {connect} from 'react-redux'
-
-import VoiceOverOff from '@material-ui/icons/VoiceOverOff'
-import RecordVoiceOver from '@material-ui/icons/RecordVoiceOver'
-import Brush from '@material-ui/icons/Brush'
-import Pencil from 'mdi-material-ui/Pencil'
-import Eraser from 'mdi-material-ui/Eraser'
-import Hand from 'mdi-material-ui/Hand'
-import PencilOff from 'mdi-material-ui/PencilOff'
-import Clear from '@material-ui/icons/Clear'
-import {
-  Button,
-  Dialog,
-  DialogContent,
-  DialogContentText
-} from '@material-ui/core/'
-import Save from '@material-ui/icons/Save'
-import Drawer from '@material-ui/core/Drawer'
 
 import Buttons from './buttons'
-
-import voiceRecognition, {isChrome} from '../utils/speechUtil'
-
-import store, {getCommand, toggleDraw, toggleErase, toggleVoice} from '../store'
-
-import BrushOptions from './brushOptions'
-import ColorPicker from './colorPicker'
-import BodyPartOptions from './bodyPartOptions'
 
 class Toolbar extends Component {
   render() {
--- client/utils/speechUtil.js
+++ client/utils/speechUtil.js
@@ -1,25 +1,78 @@
-import store, {toggleDraw, toggleErase, toggleVoice} from '../store'
+import store, {toggleDraw, toggleErase} from '../store'
 import {getCommand} from '../store/paintTools'
+import {clearCanvas} from './draw'
 
 export var isChrome =
   !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)
 
 var SpeechRecognition
-var SpeechGrammarList
-var SpeechRecognitionEvent
 
 if (isChrome) {
   SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-  SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-  SpeechRecognitionEvent =
-    SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 }
+
+const wordsThatSoundLikeStart = [
+  'smart',
+  'starred',
+  'stored',
+  'star',
+  'starts',
+  'starch',
+  'Stuart',
+  'sort',
+  'store'
+]
+const wordsThatSoundLikeStop = [
+  'step',
+  'steep',
+  'stoop',
+  'stope',
+  'swap',
+  'stopped',
+  'stops',
+  'stab',
+  'stub',
+  'sop',
+  'stomp',
+  'stump'
+]
+const wordsThatSoundLikeUndo = [
+  'unto',
+  'indu',
+  'into',
+  'anew',
+  'and',
+  'ado',
+  'two',
+  'to',
+  'too',
+  'new',
+  'untrue',
+  'hindu'
+]
+const wordsThatSoundLikeClear = [
+  'claire',
+  'queer',
+  'cleared',
+  'clearer',
+  'clears',
+  'cleary',
+  'kir',
+  'kyr',
+  'keer',
+  'care',
+  'clay',
+  'clean'
+]
 
 /*eslint-disable*/
 export const evaluateCommand = (command, state) => {
   try {
     if (state.voiceModeOn === true) {
-      if (command === 'start') {
+      if (
+        command === 'start' ||
+        wordsThatSoundLikeStart.indexOf(command) > -1
+      ) {
         //If drawMode is off, turn it on.
         if (state.drawModeOn === false) {
           store.dispatch(toggleDraw())
@@ -29,20 +82,34 @@ export const evaluateCommand = (command, state) => {
         if (state.eraseModeOn === true) {
           store.dispatch(toggleErase())
         }
-      } else if (command === 'stop') {
+      } else if (
+        command === 'stop' ||
+        wordsThatSoundLikeStop.indexOf(command) > -1
+      ) {
         if (state.drawModeOn === true) {
           store.dispatch(toggleDraw())
         }
         if (state.eraseModeOn === true) {
           store.dispatch(toggleErase())
         }
-      } else if (command === 'erase') {
+      } else if (
+        command === 'undo' ||
+        wordsThatSoundLikeUndo.indexOf(command) > -1
+      ) {
         if (state.eraseModeOn === false) {
           store.dispatch(toggleErase())
         }
-        if (state.drawModeOn === false && command === 'erase') {
+        if (
+          state.drawModeOn === false &&
+          (command === 'undo' || wordsThatSoundLikeUndo.indexOf(command) > -1)
+        ) {
           store.dispatch(toggleDraw())
         }
+      } else if (
+        command === 'clear' ||
+        wordsThatSoundLikeClear.indexOf(command) > -1
+      ) {
+        clearCanvas()
       } else {
         console.log(`I'm confused.`)
       }
@@ -63,6 +130,7 @@ export default function voiceRecognition(state) {
     recognition.start()
     recognition.onresult = function(event) {
       let speechResult = event.results[0][0].transcript.toLowerCase()
+      console.log(event.results)
       console.log(
         'You said:',
         speechResult,
